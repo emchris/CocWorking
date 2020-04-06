de6691fd7fd9880f501996f097f1d3d34c89b29e
Emmanuel Christophe <emmanuel.christophe@ec-m.fr>
2020-04-06 11:50:28 +0200

    Aggiunti CalendarView al Gradle e CocworkingApp al Manifest

--- app/src/main/java/com/example/cocworking/CocworkingApp.kt
+++ app/src/main/java/com/example/cocworking/CocworkingApp.kt
@@ -1,4 +1,16 @@
 package com.example.cocworking
 
-class CocworkingApp {
+import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
+
+
+class CocworkingApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Initialize ThreeTenABP library for backward compatibility of java.time classes (added in Java 8) used in calendar
+        AndroidThreeTen.init(this)
+    }
+
 }
