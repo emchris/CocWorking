e53e2778e0bbe9aa657e96336ccb07c805d014ed
adalgisarabino <adalgisarabino@gmail.com>
2020-05-08 17:39:52 +0200

    grafica home

--- app/src/main/java/com/example/cocworking/Retrofit/IMyService.kt
+++ app/src/main/java/com/example/cocworking/Retrofit/IMyService.kt
@@ -7,11 +7,12 @@ import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface IMyService { //in questa interfaccia definisco tutte le api calls, faccio ritornare delle call
-    @POST("register")
+    @POST("register") //definisco l'url a cui questa funzione fa riferimento
     @FormUrlEncoded
+    //dichiaro una funzione che richiamera la post relativa alla regsitrazione scritta nell'app express
     fun registerUser(@Field("email") email: String,
                      @Field("name") name: String,
-                     @Field("password") password: String) : Call<String> //una Call è una chiamata potenzialmente eseguibile che "impacchetta" qualcosa, in questo caso una stringa
+                     @Field("password") password: String) : Call<String> //una Call è un oggetto e una chiamata potenzialmente eseguibile che "impacchetta" qualcosa, in questo caso una stringa
 
     @POST("login")
     @FormUrlEncoded
